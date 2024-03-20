e3588772193ecf4419cb16499b640c23a0760434
martinsgabi <gabi.martinssilva26@gmail.com>
2024-03-20 15:38:15 -0300

    Primeiro dia, tela livro

---
+++ src/Livros.js
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+import { View, Text, FlatList, TouchableOpacity, Modal, Button, StyleSheet } from 'react-native';
+
+const Livros = () => {
+  const [books, setBooks] = useState([
+    { id: 1, title: "Livro 1", description: "O pequeno príncipe é um clássico da literatura infantil que narra a amizade entre um menino e um piloto de avião. O principezinho vem do asteroide B 612, e encontra o piloto no deserto do Saara. A obra fala de amor, amizade e sobre a sabedoria infantil." },
+    { id: 2, title: "Livro 2", description: "Descrição do Livro 2" },
+    { id: 3, title: "Livro 3", description: "Descrição do Livro 3" },
+    // Adicione mais livros conforme necessário
+  ]);
+  const [selectedBook, setSelectedBook] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handleBookPress = (book) => {
+    setSelectedBook(book);
+    setModalVisible(true);
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => handleBookPress(item)} style={styles.bookItem}>
+      <Text style={styles.bookTitle}>{item.title}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Biblioteca de Livros</Text>
+
+      <FlatList
+        data={books}
+        renderItem={renderItem}
+        keyExtractor={item => item.id.toString()}
+      />
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => setModalVisible(false)}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>{selectedBook ? selectedBook.title : ''}</Text>
+            <Text style={styles.modalDescription}>{selectedBook ? selectedBook.description : ''}</Text>
+            <Button title="Fechar" onPress={() => setModalVisible(false)} />
+          </View>
+        </View>
+      </Modal>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#fff',
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  bookItem: {
+    paddingVertical: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+  },
+  bookTitle: {
+    fontSize: 18,
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    backgroundColor: '#fff',
+    padding: 20,
+    borderRadius: 10,
+    width: '80%',
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  modalDescription: {
+    fontSize: 18,
+    marginBottom: 10,
+  },
+});
+
+export default Livros;
