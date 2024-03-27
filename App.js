cb7d3369a8cec60cbe9929b610b07654b4bd17ef
isambarboza <isabelabarboza17@gmail.com>
2024-03-27 10:35:32 -0300

    2 dia de projeto - pagina de login

--- App.js
+++ App.js
@@ -1,27 +1,12 @@
-import { StyleSheet } from 'react-native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import { NavigationContainer } from '@react-navigation/native';
-import Login from './src/Login';
-import Livros from './src/Livros';
-
-
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
-const Tab = createBottomTabNavigator();
+import 'react-native-gesture-handler';
+import UserProvider from "./src/Context/UserContext";
+import Rotas from "./src/Rotas";
 
 export default function App() {
+
   return (
-    <NavigationContainer>
-      <Tab.Navigator 
-      screenOptions={{
-        tabBarStyle:{backgroundColor:"#1A237E"},
-        headerStyle: { backgroundColor: "#1A237E"}
-      }
-        }>
-        <Tab.Screen name=" " component={Login} options={{ tabBarIcon: ({ color, size }) => (
-            <MaterialCommunityIcons name="account" color={color} size={size} />
-        ),}} />
-      </Tab.Navigator>   
-    </NavigationContainer>
+    <UserProvider>
+      <Rotas />
+    </UserProvider>
   );
 }
---
+++ src/Rotas.js
@@ -0,0 +1,34 @@
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+
+import Login from './Login';
+import Livros from './Livros';
+import { useContext } from 'react';
+import { UserContext } from './Context/UserContext';
+
+const Tab = createBottomTabNavigator();
+
+export default function Rotas() {
+
+    const {logado} = useContext( UserContext );
+
+    if( logado == false ) {
+        return( <Login />)
+    }
+
+    return (
+        <NavigationContainer>
+      <Tab.Navigator 
+      screenOptions={{
+        tabBarStyle:{backgroundColor:"#1A237E"},
+        headerStyle: { backgroundColor: "#1A237E"}
+      }
+        }>
+          <Tab.Screen name=" " component={Livros} options={{ tabBarIcon: ({ color, size }) => (
+            <MaterialCommunityIcons name="book-open-page-variant-outline" color={color} size={size} />
+        ),}} />
+      </Tab.Navigator>   
+    </NavigationContainer>
+    )
+}
